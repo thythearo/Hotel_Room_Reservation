d74800a5bcb7cd413544f8cf7677144895e115a5
thythearo <thythearo200621@gmail.com>
2026-02-03 13:19:50 +0700

    first commit

---
+++ Login/DashboardLogin.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto+Flex:opsz,wght,XOPQ,XTRA,YOPQ,YTDE,YTFI,YTLC,YTUC@8..144,100..1000,96,468,79,-203,738,514,712&display=swap" rel="stylesheet">
+  <!-- icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+  <!-- Swiper js -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
+
+  <!-- new font add -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+    <style>
+        *{
+            font-family: "Noto Sans", sans-serif;
+            
+        }
+        .h-font{
+          font-family: "Noto Sans", sans-serif;
+        }
+
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+          -webkit-appearance: none; 
+          margin: 0;                
+        }
+
+        input[type=number] {
+          -moz-appearance: textfield; 
+        }
+
+        .banner-img {
+          width: 95%;
+          height: 350px;     /* 👈 change this value */
+          object-fit: cover;
+          border-radius: 20px;
+            /* keeps image nice */
+        }
+        .booking-box {
+          margin-top: -50px; /* 👈 exactly what you want */
+          z-index: 10;
+        }
+        @media screen and (max-width: 575px) {
+          .booking-box {
+            margin-top: 0px;
+            z-index: 10;
+
+          }
+          .banner-img {
+            width: 85%;
+            height: 200px;    
+            object-fit: cover;
+            border-radius: 0;
+
+          }
+        }
+        .booking-card{
+          border-radius: 20px 20px 0 0;
+        }
+        .custom-bg{
+          background-color: #2ec1ac;
+        }
+        .custom-bg:hover{
+          background-color: #279e8c;
+        }
+    </style>
+</head>
+<body class="bg-light">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+    <div class="container-fluid">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="DashboardLogin.php">Ai Hotel</a>
+    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#">Rooms</a>
+        </li class="nav-item">
+          <a class="nav-link me-2" href="#">Facilities</a>
+        </li class="nav-item">
+          <a class="nav-link me-2" href="#">Contact Us</a>
+        </li class="nav-item">
+          <a class="nav-link me-2" href="#">About</a>
+        </li>
+
+      </ul>
+      <div class="d-flex" class="menu-login">
+          <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+          Login
+          </button>
+          <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal" data-bs-target="#registerModal">
+          Register
+          </button>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+<div class="modal fade" id="loginModal" data-bs-backdrop= "static" data-bs-keyboard= "false" tabindex="-1" >
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <form>
+        <div class="modal-header">
+          <h5 class="modal-title">
+            <i class="bi bi-person-circle fs-3 me-2 "></i>
+            User Login
+          </h5>
+          <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+              <label class="form-label">Email address</label>
+              <input type="email" class="form-control shadow-none">
+          </div>
+          <div class="mb-4">
+              <label class="form-label">Password</label>
+              <input type="password" class="form-control shadow-none">
+          </div>
+          <div class="d-flex align-item-center justify-content-between mb-2">
+            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="registerModal"  data-bs-backdrop= "static" data-bs-keyboard= "false" tabindex="-1">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <form>
+        <div class="modal-header">
+          <h5 class="modal-title d-flex align-items-center">
+            <i class="bi bi-person-lines-fill fs-3 me-2"></i>
+            User Register
+          </h5>
+        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+
+        <div class="modal-body">
+            <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+              Note  : Your Details  must much with your ID (Aadhar Card, Passport , Driving license, etc.)  That will be required during check-in
+            </span>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Name</label>
+                  <input type="text" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Email</label>
+                  <input type="email" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Phone Number</label>
+                  <input type="number" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Picture</label>
+                  <input type="file" class="form-control shadow-none">
+                </div>
+                 <div class="col-md-12 p-0 mb-3">
+                  <label class="form-label">Address</label>
+                  <textarea class="form-control shadow-none" rows="3"></textarea>
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Pincode</label>
+                  <input type="number" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Date of Birth</label>
+                  <input type="date" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Password</label>
+                  <input type="password" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Comfirm Password</label>
+                  <input type="password" class="form-control shadow-none">
+                </div>
+              </div>
+            </div>
+            <div class="text-center my-1">
+              <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
+            </div>
+
+
+
+
+       
+         
+          <!-- <div class="mb-3">
+              <label class="form-label">Email address</label>
+              <input type="email" class="form-control shadow-none">
+          </div>
+          <div class="mb-4">
+              <label class="form-label">Password</label>
+              <input type="password" class="form-control shadow-none">
+          </div>
+          <div class="d-flex align-item-center justify-content-between mb-2">
+            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+          </div> -->
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Swiper js -->
+  <div class="container-fluid px-lg-4 mt-3">
+    <div class="swiper swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="/hotel_room_reservation/images/hotel-1.jpg" class="img-fluid banner-img d-block mx-auto" />
+        </div>
+        <div class="swiper-slide">
+          <img src="/hotel_room_reservation/images/hotel-2.jpg" class="img-fluid banner-img d-block mx-auto" />
+        </div>
+        <div class="swiper-slide">
+          <img src="/hotel_room_reservation/images/hotel-3.jpg" class="img-fluid banner-img d-block mx-auto" />
+        </div>
+        <div class="swiper-slide">
+          <img src="/hotel_room_reservation/images/hotel.4.jpg" class="img-fluid banner-img d-block mx-auto" />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Check Availability Form -->
+   <div class="container booking-box position-relative">
+    <div class="row justify-content-center">
+      <div class="col-lg-12 bg-white shadow p-4 rounded booking-card">
+        <h5 class="mb-4">Check Booking Availability</h5>
+        <form>
+          <div class="row align-items-end">
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight: 500;">Check in</label>
+              <input type="date" class="form-control shadow-none">
+            </div>
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight: 500;">Check Out</label>
+              <input type="date" class="form-control shadow-none">
+            </div>
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight: 500;">Adult</label>
+              <select class="form-select shadow-none">
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div class="col-lg-2 mb-3">
+              <label class="form-label" style="font-weight: 500;">Children</label>
+              <select class="form-select shadow-none">
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div class="col-lg-1 mb-lg-3 mt-2">
+              <button type="submit" class="btn text-white shadow-none custom-bg">submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+   </div>
+
+   <!-- Our Room -->
+  <h2 class="mt-1 pt-4 mb-4 text-center fw-bold">Our Rooms</h2>
+
+   <br><br><br>
+   <br><br><br>
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+    <script>
+      var swiper = new Swiper(".swiper-container", {
+        spaceBetween: 30,
+        effect: "fade",
+        loop: true,
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+        }
+      });
+    </script>
+</body>
+</html>
